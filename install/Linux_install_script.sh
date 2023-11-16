1d6d8ab74867b0323d86ad31b7f3c8ec9fdf2dc1
Domotrique <148430940+Domotrique@users.noreply.github.com>
2023-11-16 17:18:12 +0100

    Create Linux_install_script.sh
    To install from a script

---
+++ install/Linux_install_script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt-get -y install mariadb-server
+sudo mysql -e "CREATE USER 'okouser'@'localhost' IDENTIFIED BY 'okopass';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'okouser'@'localhost' ;"
+sudo apt-get -y install apache2
+sudo systemctl enable apache2
+
+sudo apt update
+sudo apt-get -y install php8.2 php8.2-cli php8.2-common
+sudo apt-get -y install php8.2-mysql php8.2-mbstring php8.2-xml php8.2-curl php8.2-gd php8.2-mbstring php8.2-intl php8.2-zip
+
+cd /var/www/
+sudo wget https://github.com/domotrique/okovision_2023/archive/master.zip
+sudo unzip master.zip
+[ -d "/path/to/dir" ] && mv okovision/ "$(date +"%y-%m-%d")_okovision"
+sudo mv okovision-master/ okovision/
+sudo rm master.zip
+sudo chown www-data:www-data -R okovision/
+sudo cp /var/www/okovision/install/099-okovision.conf /etc/apache2/sites-available/.
+sudo a2ensite 099-okovision.conf
+sudo a2dissite 000-default
+sudo service apache2 reload
+
